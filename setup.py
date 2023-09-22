1aec8db52c0835971d3e58cdee65ad22e92349c8
Itay Shoshani <5647956+itay-sho@users.noreply.github.com>
2023-09-22 12:20:52 -0400

    bumping temper version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='temper',
-    version='0.0.1',
+    version='0.0.2',
     description='A simple CLI tool to create temporary directories',
     author='Itay Shoshani',
     install_requires=[
